2882d6afbb4013bfb06725253651e26acd964efe
Michael Theis (msg) <michael.theis@msg.group>
2022-08-12 18:44:29 +0200

    released version 5.2.0
    - consolidated application-specific prometheus metric names with other showcases

--- changelog.md
+++ changelog.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [5.2.0] - 2022-08-12
+### Changed
+- consolidated application-specific prometheus metric names with other showcases
+
 ## [5.1.0] - 2021-11-11
 ### Added
 ### Changed
--- src/main/java/group/msg/at/cloud/cloudtrain/core/boundary/TaskManagement.java
+++ src/main/java/group/msg/at/cloud/cloudtrain/core/boundary/TaskManagement.java
@@ -3,17 +3,18 @@ package group.msg.at.cloud.cloudtrain.core.boundary;
 import group.msg.at.cloud.cloudtrain.adapter.persistence.jpa.repository.GenericRepository;
 import group.msg.at.cloud.cloudtrain.core.control.UserPermissionVerifier;
 import group.msg.at.cloud.cloudtrain.core.entity.Task;
-import org.eclipse.microprofile.metrics.annotation.Counted;
-import org.eclipse.microprofile.metrics.annotation.Gauge;
-import org.eclipse.microprofile.metrics.annotation.Timed;
-
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.SimplyTimed;
+
 import java.util.List;
 import java.util.UUID;
+
+import static group.msg.at.cloud.cloudtrain.MetricsConstants.*;
 
 /**
  * Simple {@code Boundary} that manages {@code Task} entities.
@@ -28,9 +29,16 @@ public class TaskManagement {
     @Inject
     private UserPermissionVerifier verifier;
 
+    @Counted(name = BUSINESS_OPERATION_METRIC_COUNT_NAME,
+            description = BUSINESS_OPERATION_METRIC_COUNT_DESCRIPTION,
+            absolute = true,
+            tags = {BUSINESS_OPERATION_NAME_TAG + "=task_add"})
+    @SimplyTimed(name = BUSINESS_OPERATION_METRIC_TIME_NAME,
+            description = BUSINESS_OPERATION_METRIC_TIME_DESCRIPTION,
+            absolute = true,
+            tags = {BUSINESS_OPERATION_NAME_TAG + "=task_add"},
+            unit = "seconds")
     @NotNull
-    @Counted(name = "business_task_added")
-    @Timed(name="business_task_added_time")
     public UUID addTask(@NotNull @Valid Task newTask) {
         verifier.requirePermission("TASK_CREATE");
         newTask.setId(UUID.randomUUID());
@@ -38,21 +46,58 @@ public class TaskManagement {
         return newTask.getId();
     }
 
+    @Counted(name = BUSINESS_OPERATION_METRIC_COUNT_NAME,
+            description = BUSINESS_OPERATION_METRIC_COUNT_DESCRIPTION,
+            absolute = true,
+            tags = {BUSINESS_OPERATION_NAME_TAG + "=task_modify"})
+    @SimplyTimed(name = BUSINESS_OPERATION_METRIC_TIME_NAME,
+            description = BUSINESS_OPERATION_METRIC_TIME_DESCRIPTION,
+            absolute = true,
+            tags = {BUSINESS_OPERATION_NAME_TAG + "=task_modify"},
+            unit = "seconds")
     public void modifyTask(@NotNull @Valid Task modifiedTask) {
         verifier.requirePermission("TASK_UPDATE");
         this.repository.setEntity(modifiedTask);
     }
 
+    @Counted(name = BUSINESS_OPERATION_METRIC_COUNT_NAME,
+            description = BUSINESS_OPERATION_METRIC_COUNT_DESCRIPTION,
+            absolute = true,
+            tags = {BUSINESS_OPERATION_NAME_TAG + "=task_get"})
+    @SimplyTimed(name = BUSINESS_OPERATION_METRIC_TIME_NAME,
+            description = BUSINESS_OPERATION_METRIC_TIME_DESCRIPTION,
+            absolute = true,
+            tags = {BUSINESS_OPERATION_NAME_TAG + "=task_get"},
+            unit = "seconds")
     public Task getTaskById(@NotNull UUID taskId) {
         verifier.requirePermission("TASK_READ");
         return this.repository.getEntityById(Task.class, taskId);
     }
 
+    @Counted(name = BUSINESS_OPERATION_METRIC_COUNT_NAME,
+            description = BUSINESS_OPERATION_METRIC_COUNT_DESCRIPTION,
+            absolute = true,
+            tags = {BUSINESS_OPERATION_NAME_TAG + "=task_delete"})
+    @SimplyTimed(name = BUSINESS_OPERATION_METRIC_TIME_NAME,
+            description = BUSINESS_OPERATION_METRIC_TIME_DESCRIPTION,
+            absolute = true,
+            tags = {BUSINESS_OPERATION_NAME_TAG + "=task_delete"},
+            unit = "seconds")
     public void removeTask(@NotNull UUID taskId) {
         verifier.requirePermission("TASK_DELETE");
         this.repository.removeEntityById(Task.class, taskId);
     }
 
+    @Counted(name = BUSINESS_OPERATION_METRIC_COUNT_NAME,
+            description = BUSINESS_OPERATION_METRIC_COUNT_DESCRIPTION,
+            absolute = true,
+            tags = {BUSINESS_OPERATION_NAME_TAG + "=task_browse"})
+    @SimplyTimed(name = BUSINESS_OPERATION_METRIC_TIME_NAME,
+            description = BUSINESS_OPERATION_METRIC_TIME_DESCRIPTION,
+            absolute = true,
+            tags = {BUSINESS_OPERATION_NAME_TAG + "=task_browse"},
+            unit = "seconds")
+    @NotNull
     public List<Task> getAllTasks() {
         verifier.requirePermission("TASK_READ");
         return this.repository.queryEntities(Task.class, Task.QUERY_ALL, null);
